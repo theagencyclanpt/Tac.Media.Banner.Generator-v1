640efa10a69c2a671eedd33c18560840d1fac72e
Diogo Ribeiro [1201768] <1201768@isep.ipp.pt>
2021-07-20 16:07:02 +0100

    auth with cookies still not working

--- index.js
+++ index.js
@@ -57,23 +57,88 @@ app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login", "login.html"));
 });
 
-
-function signIn(userName, password){
-  app.use('/login', (req,res) =>{
-
-    login(req, res, userName, password);
-    //let myStatus =
-    if (res !== 401) {
-      app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "public", "index.html"));
-      })
+app.post('/api/auth', (req, res) => {
+  let cookie_Stuff=req.signedCookies.user  //But the user is logging in for the first time so there won't be any appropriate signed cookie for usage.
+  if(!cookie_Stuff) {
+    let auth_Stuff=req.headers.authorization
+    if(!auth_Stuff)//No authentication info given
+    {
+      res.setHeader("WWW-Authenticate", "Basic")
+      res.sendStatus(401)
+    } else {
+      step1 = new Buffer.from(auth_Stuff.split(" ")[1], 'base64')
+      //Extracting username:password from the encoding Authorization: Basic username:password
+      step2 = step1.toString().split(":")
+      //Extracting the username and password in an array
+      if(step2[0]=='admin' && step2[1]=='admin') {
+        //Correct username and password given
+        console.log("WELCOME ADMIN")
+        //Store a cookie with name=user and value=username
+        res.cookie('user', 'admin', {signed: true})
+        res.send("Signed in the first time")
+      } else {
+        //Wrong authentication info, retry
+        res.setHeader("WWW-Authenticate", "Basic")
+        res.sendStatus(401)
+      }
     }
-  })
-}
+  } else {//Signed cookie already stored
+    if(req.signedCookies.user=='admin') {
+      res.send("HELLO GENUINE USER")
 
-/*app.get("/", (req, res) => {
+    } else {
+      //Wrong info, user asked to authenticate again
+      res.setHeader("WWW-Authenticate", "Basic")
+      res.sendStatus(401)
+    }
+  }
+})
+
+/*app.use('/login', (req,res) =>{
+  let cookie_Stuff=req.signedCookies.user  //But the user is logging in for the first time so there won't be any appropriate signed cookie for usage.
+  if(!cookie_Stuff) {
+    let auth_Stuff=req.headers.authorization
+    if(!auth_Stuff)//No authentication info given
+    {
+      res.setHeader("WWW-Authenticate", "Basic")
+      res.sendStatus(401)
+    } else {
+      step1 = new Buffer.from(auth_Stuff.split(" ")[1], 'base64')
+      //Extracting username:password from the encoding Authorization: Basic username:password
+      step2 = step1.toString().split(":")
+      //Extracting the username and password in an array
+      if(step2[0]==userName && step2[1]==password) {
+        //Correct username and password given
+        console.log("WELCOME ADMIN")
+        //Store a cookie with name=user and value=username
+        res.cookie('user', 'admin', {signed: true})
+        res.send("Signed in the first time")
+      } else {
+        //Wrong authentication info, retry
+        res.setHeader("WWW-Authenticate", "Basic")
+        res.sendStatus(401)
+      }
+    }
+  } else {//Signed cookie already stored
+    if(req.signedCookies.user==userName) {
+      res.send("HELLO GENUINE USER")
+
+    } else {
+      //Wrong info, user asked to authenticate again
+      res.setHeader("WWW-Authenticate", "Basic")
+      res.sendStatus(401)
+    }
+  }
+  if (res !== 401) {
+    app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "public", "index.html"));
+    })
+  }
+})*/
+
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
-});*/
+});
 
 
 app.get("/calendar", (req, res) => {
--- public/login/script.js
+++ public/login/script.js
@@ -1,43 +1,24 @@
-const userNameInput = document.getElementById('UserUsername');
-const passwordInput = document.getElementById('UserPassword');
 
+function login(){
+    const userNameInput = document.getElementById('UserUsername').value;
+    const passwordInput = document.getElementById('UserPassword').value;
 
-function login(req, res, userName, password){
-    let cookie_Stuff=req.signedCookies.user  //But the user is logging in for the first time so there won't be any appropriate signed cookie for usage.
-    if(!cookie_Stuff) {
-        let auth_Stuff=req.headers.authorization
-        if(!auth_Stuff)//No authentication info given
-        {
-            res.setHeader("WWW-Authenticate", "Basic")
-            res.sendStatus(401)
-        } else {
-            step1 = new Buffer.from(auth_Stuff.split(" ")[1], 'base64')
-            //Extracting username:password from the encoding Authorization: Basic username:password
-            step2 = step1.toString().split(":")
-            //Extracting the username and password in an array
-            if(step2[0]==userName && step2[1]==password) {
-                //Correct username and password given
-                console.log("WELCOME ADMIN")
-                //Store a cookie with name=user and value=username
-                res.cookie('user', 'admin', {signed: true})
-                res.send("Signed in the first time")
-            } else {
-                //Wrong authentication info, retry
-                res.setHeader("WWW-Authenticate", "Basic")
-                res.sendStatus(401)
+    fetch("/api/auth", {
+        method: "POST",
+        headers: { "Content-type": "application/json;charset=UTF-8" },
+        credentials: "include",
+        body: JSON.stringify({ userName: userNameInput, password: passwordInput } ),
+    })
+        .then((response) => {
+            console.log(response.signedCookies)
+            if ( !(response.status >= 200 && response.status < 300) ) {
+                var error = new Error(response.statusText)
+                throw error
             }
-        }
-    } else {//Signed cookie already stored
-        if(req.signedCookies.user==userName) {
-            //res.send("HELLO GENUINE USER")
-
-        } else {
-            //Wrong info, user asked to authenticate again
-            res.setHeader("WWW-Authenticate", "Basic")
-            res.sendStatus(401)
-        }
-    }
-    //return res;
+        })
+        .then((r) => {
+            window.location.href = '/';
+        });
 }
 
 
